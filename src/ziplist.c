07fe645ea8131d22c3ab3fa1f9d409fc3a0ab0a3
nullcc <nullcc@gmail.com>
2017-11-16 17:43:39 +0800

    update

--- src/ziplist.c
+++ src/ziplist.c
@@ -1146,6 +1146,7 @@ unsigned char *ziplistDelete(unsigned char *zl, unsigned char **p) {
 }
 
 /* Delete a range of entries from the ziplist. */
+/* 删除ziplist中一个范围内的节点 */
 unsigned char *ziplistDeleteRange(unsigned char *zl, int index, unsigned int num) {
     unsigned char *p = ziplistIndex(zl,index);
     return (p == NULL) ? zl : __ziplistDelete(zl,p,num);
